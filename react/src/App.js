b77f7d7431bc5ac5b86422c90958b03544879c72
dynepark-dev <dynepark.dev@gmail.com>
2022-04-02 00:27:32 +0900

    feat: FE: alert ribbon

--- react/src/App.js
+++ react/src/App.js
@@ -8,12 +8,18 @@ import Webtoons from "./Pages/Webtoons";
 import Home from "./Pages/Home";
 import NotFound from "./Pages/NotFound";
 import HoverButton from "./Components/HoverButton";
+import Ribbon from "./Components/Ribbon";
 
 function App() {
   return (
     <div className="App">
       {/* <Aside /> */}
       <Navbar />
+      <Ribbon
+        line1="Sign Up now and save your favorite webtoons."
+        line2="Sign Up ›"
+        link="#"
+      />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
