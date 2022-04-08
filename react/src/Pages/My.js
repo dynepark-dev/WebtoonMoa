154dfff6c9939f38b47b9cf3e60787d53b59f66b
dynepark-dev <dynepark.dev@gmail.com>
2022-04-08 23:22:19 +0900

    feat: FE mypage

---
+++ react/src/Pages/My.js
@@ -0,0 +1,34 @@
+import React from "react";
+import HorizontalLine from "../Components/HorizontalLine";
+import styles from "../Styles/My.module.scss";
+import data from "../DB/database.json";
+import WebtoonsList from "../Components/WebtoonsList";
+import useToggle from "../Hooks/useToggle";
+
+function My() {
+  const webtoon = data.naver;
+  const fav = [...webtoon].reverse().splice(0, 22);
+  const trash = [...webtoon].splice(1, 22);
+  const history = [...webtoon].splice(20, 22);
+
+  const [showTrash, toggleTrash] = useToggle(false);
+  return (
+    <div className={styles.My}>
+      <div className={styles.wrapper}>
+        <h2>My Page</h2>
+        <HorizontalLine content="찜한웹툰" />
+        <WebtoonsList data={fav} />
+        <HorizontalLine content="방금본웹툰" />
+        <WebtoonsList data={history} />
+        <HorizontalLine
+          content={showTrash ? "숨긴웹툰 숨김 ▲" : "숨긴웹툰 보기 ▼"}
+          onClick={() => toggleTrash()}
+          style={{ cursor: "pointer" }}
+        />
+        {showTrash && <WebtoonsList data={trash} />}
+      </div>
+    </div>
+  );
+}
+
+export default My;
--- react/src/Pages/Webtoons.js
+++ react/src/Pages/Webtoons.js
@@ -2,12 +2,43 @@ import React from "react";
 import styles from "../Styles/Webtoons.module.scss";
 import db from "../DB/database.json";
 import WebtoonsList from "../Components/WebtoonsList";
+import Button from "../Components/Button";
+import Filter from "../Components/Filter";
 
 function Webtoons() {
+  const platformFilter = [
+    "네이버",
+    "카카오",
+    "카카오페이지",
+    "머시기",
+    "저시기",
+    "거시기",
+    "저머시기",
+  ];
+  const genreFilter = [
+    "로맨스",
+    "드라마",
+    "판타지",
+    "액션",
+    "무협",
+    "개그",
+    "학원",
+    "일상",
+    "기타",
+  ];
   const data = db.webtoons;
   return (
     <div className={styles.Webtoons}>
-      <WebtoonsList data={data} />
+      <div className={styles.wrapper}>
+        <h2>Ongoing Webtoons</h2>
+        <div className={styles.filters}>
+          <Filter initial={["전체"]} array={platformFilter} clear="전체" />
+          <Filter initial={["전체"]} array={genreFilter} clear="전체" />
+        </div>
+        <WebtoonsList data={data} />
+        <Button children="더보기" />
+        {/* <Button children="마지막페이지 입니다" /> */}
+      </div>
     </div>
   );
 }
